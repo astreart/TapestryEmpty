1a432561f07f38ae1546089b903d15eb0aa9d9f7
Astreart <astreart@gmail.com>
2014-01-25 22:59:49 +0200

    ch8, page 205

--- src/com/musala/tapestry/tutorial/components/Border.java
+++ src/com/musala/tapestry/tutorial/components/Border.java
@@ -2,10 +2,18 @@ package com.musala.tapestry.tutorial.components;
 
 import java.util.Locale;
 
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.PersistentLocale;
+
+import com.musala.tapestry.tutorial.pages.AddCelebrity;
+import com.musala.tapestry.tutorial.pages.Index;
+import com.musala.tapestry.tutorial.pages.Registration;
+import com.musala.tapestry.tutorial.pages.ShowAll;
 
 public class Border {
 	@Inject
@@ -19,7 +27,7 @@ public class Border {
 		if (localeLabel == null) {
 			Locale.setDefault(Locale.ENGLISH);
 		}
-		
+
 		if (currentLocale.equals(Locale.GERMAN)) {
 			localeLabel = new Locale("en").getDisplayName(Locale.ENGLISH);
 		} else {
@@ -37,4 +45,36 @@ public class Border {
 			persistentLocale.set(Locale.GERMAN);
 		}
 	}
+
+	@Inject
+	private ComponentResources resources;
+
+	public String getPageName() {
+		org.apache.tapestry5.runtime.Component page = resources.getContainer();
+		return page.getClass().getName();
+	}
+
+	public boolean isNotIndex() {
+		return !getPageName().equals(Index.class.getName());
+	}
+
+	public boolean isRegistration() {
+		return getPageName().equals(Registration.class.getName());
+	}
+
+	public boolean isShowAll() {
+		return getPageName().equals(ShowAll.class.getName());
+	}
+
+	public boolean isAddCelebrity() {
+		return getPageName().equals(AddCelebrity.class.getName());
+	}
+
+	@Parameter(required = true, defaultPrefix = "literal")
+	private String pageTitle;
+
+	public String getPageTitle() {
+		return pageTitle;
+	}
+
 }
