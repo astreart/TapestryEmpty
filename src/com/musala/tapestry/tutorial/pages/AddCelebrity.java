3bdf4d01d357ef506f6238458aa2ff03753126a2
Astreart <astreart@gmail.com>
2014-01-26 19:33:56 +0200

    AddCelebrity.java - Committed
    May be missed

--- src/com/musala/tapestry/tutorial/pages/AddCelebrity.java
+++ src/com/musala/tapestry/tutorial/pages/AddCelebrity.java
@@ -1,31 +1,15 @@
 package com.musala.tapestry.tutorial.pages;
 
-import org.apache.tapestry5.Asset;
-
-import org.apache.tapestry5.annotations.Import;
-import org.apache.tapestry5.annotations.Path;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.SessionState;
-import org.apache.tapestry5.annotations.SupportsInformalParameters;
-import org.apache.tapestry5.ioc.annotations.Inject;
-
 import com.musala.tapestry.tutorial.data.IDataSource;
 import  com.musala.tapestry.tutorial.model.Celebrity;
 
-@SupportsInformalParameters
-@Import(stylesheet = { "context:css/styles.css" })
-
 public class AddCelebrity {
-
-	private Asset styles;
 
 	@SessionState
 	private IDataSource dataSource;
 
-	public Asset getStyles() {
-		return styles;
-	}
-	
 	@Persist
 	private Celebrity celebrity;
 
@@ -35,13 +19,11 @@ public class AddCelebrity {
 	}*/
 
 	public Celebrity getCelebrity() {
-		/*if(celebrity == null){
-			this.celebrity = new Celebrity();
-		}*/
 		return celebrity;
 	}
 
 	public void setCelebrity(Celebrity celebrity) {
 		this.celebrity = celebrity;
+	
 	}
 }
